7d07bac1720f296c6d9d4b4ed76ead8c864be0cf
motiam <40633627+motiam@users.noreply.github.com>
2023-04-30 21:18:06 +0800

    Update i18n.py

--- i18n.py
+++ i18n.py
@@ -12,11 +12,10 @@ def load_language_list(language):
 class I18nAuto:
     def __init__(self, language=None):
         if language in ["Auto", None]:
-            language = locale.getdefaultlocale()[
-                0
-            ]  # getlocale can't identify the system's language ((None, None))
+            language = "zh_CN"
+           # getlocale can't identify the system's language ((None, None))
         if not os.path.exists(f"./i18n/{language}.json"):
-            language = "en_US"
+            language = "zh_CN"
         self.language = language
         print("Use Language:", language)
         self.language_map = load_language_list(language)
